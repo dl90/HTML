cf28be64b823c80ef3bab03d621527e4406f1b2f
Don <donli90@hotmail.com>
2019-11-01 20:47:56 -0700

    updated styling for Instasam
    style.css
    updated how posts are positioned
    TODO style contents within posts
    
    index.html
    TODO refactor some tags
    
    script.js
    TODO need to implememnt javascript

---
+++ Instasam/script.js
@@ -0,0 +1,14 @@
+// const arrOfUrls = [];
+
+// function imageNode(imgURL) {
+//     const node = document.createElement("img");
+//     node.setAttribute('src', imgURL);
+//     return node;
+// }
+
+
+
+
+// const firstNode = imageNode[arrOfUrls[0]];
+
+// const mainContent = document.querySelector("#main-content");
